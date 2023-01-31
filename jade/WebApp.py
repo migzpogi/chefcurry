aad08818a21ed8262a83319de458dee0cbca487f
Miguel <migz.zgim@gmail.com>
2023-01-31 23:05:36 +0100

    Adds time in foobar

--- jade/WebApp.py
+++ jade/WebApp.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, make_response
+import time
+from datetime import datetime
+import sys
 
 app = Flask(__name__)
 
@@ -18,6 +21,16 @@ PLAIN_TEXT_AGENTS = [
 ]
 
 
+def run_clock():
+    while True:
+        now = datetime.now()
+        sys.stdout.write("\r")
+        sys.stdout.write(now.strftime("%Y-%m-%d %H:%M:%S"))
+        sys.stdout.flush()
+        time.sleep(1)
+
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -33,13 +46,44 @@ def foobar():
     user_agent = parsed_query.get('user_agent', '').lower()
     is_html = not any(agent in user_agent for agent in PLAIN_TEXT_AGENTS)
 
+    now = datetime.now()
+    now_text = now.strftime("%Y-%m-%d %H:%M:%S")
+
     if is_html:
         return render_template('index.html')
     else:
-        response = make_response("hello\n", 200)
+        time_text = f"""
+        Local : {now_text}       LA: some time
+        Manila: {now_text}       NY: some time
+        """
+        response = make_response(f"{time_text}\n", 200)
+        response.mimetype = "text/plain"
+        return response
+
+@app.route("/timer/<id>")
+def timer(id):
+
+    parsed_query = {
+        'user_agent': request.headers.get('User-Agent', '').lower()
+    }
+
+    user_agent = parsed_query.get('user_agent', '').lower()
+    is_html = not any(agent in user_agent for agent in PLAIN_TEXT_AGENTS)
+
+    if is_html:
+        return render_template('index.html')
+    else:
+        response = make_response(f"timer {id}\n", 200)
         response.mimetype = "text/plain"
         return response
 
 
+@app.route("/dazzle")
+def dazzle():
+    print('str', file=sys.stderr)
+    app.logger.error('str')
+    return("dazzle")
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
