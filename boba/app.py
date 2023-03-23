dc23fc3e8e45989a08b9e7517090e5c132d159b4
Miguel <migz.zgim@gmail.com>
2023-03-23 05:52:01 +0100

    Adds unit tests

--- boba/app.py
+++ boba/app.py
@@ -1,3 +1,6 @@
+import requests
+
+
 def sum(a, b):
     """
     Returns the sum of a and b
@@ -7,3 +10,16 @@ def sum(a, b):
     """
 
     return a+b
+
+
+def get_foobar():
+    """
+    Makes an HTTP GET request
+    :return:
+    """
+
+    r = requests.get("https://www.google.com")
+    return r.status_code
+
+
+print(get_foobar())
--- tests/test_boba.py
+++ tests/test_boba.py
@@ -1,4 +1,5 @@
 import unittest
+import responses
 from boba import app
 
 
@@ -6,6 +7,23 @@ class TestBobaApp(unittest.TestCase):
 
     def test_sum(self):
         self.assertTrue(app.sum(1, 2), 3)
+
+    @responses.activate
+    def test_foobar(self):
+        """
+        See https://stackoverflow.com/questions/9559963/unit-testing-a-python-app-that-uses-the-requests-library
+        :return:
+        """
+        responses.add(**{
+            'method': responses.GET,
+            'url': 'https://www.google.com',
+            'body': '{"error": "reason"}',
+            'status': 500,
+            'content_type': 'application/json',
+            'adding_headers': {'X-Foo': 'Bar'}
+        })
+
+        self.assertEqual(app.get_foobar(), 500)
 
 
 if __name__ == '__main__':
